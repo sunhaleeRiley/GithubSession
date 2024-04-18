d76ee1fa81fa29c31b709913ab51e31b717ad1fc
sunha <artistsh99@gmail.com>
2024-04-18 20:14:24 +0900

    Initial Commit

---
+++ GithubSession/GithubSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSessionApp.swift
+//  GithubSession
+//
+//  Created by Sunha on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
